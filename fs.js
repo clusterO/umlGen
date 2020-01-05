dae389d4c17ac79f6534f42186b43ab26ff65729
cluserO <fourmou.m@gmail.com>
2020-01-05 20:08:57 +0100

    read file & test locations

--- fs.js
+++ fs.js
@@ -3,12 +3,26 @@ const fs = require("fs");
 
 let browseDirectory = dirPath => {
   fs.readdir(dirPath, (err, files) => {
+    if (err) console.error(err);
     files.forEach(file => {
       fs.statSync(path.join(dirPath, file)).isDirectory()
         ? browseDirectory(path.join(dirPath, file))
-        : console.log(file);
+        : readFile(path.join(dirPath, file));
     });
   });
+};
+
+let readFile = file => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) console.error(err);
+    if (path.basename(file) === "test.js") locateKeyword("class", data);
+  });
+};
+
+let locateKeyword = (keyword, data) => {
+  console.log(data.charAt(35));
+  console.log(data.match(/\r\n/g).length);
+  console.log(data.slice(35).indexOf("\r\n"));
 };
 
 module.exports = { browseDirectory };
